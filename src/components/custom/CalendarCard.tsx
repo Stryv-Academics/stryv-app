ebf772a993304077d60c5daf3fc2c9be5a71430c
Zachary Berkenkotter <109328944+zachberk@users.noreply.github.com>
2025-01-31 15:30:43 -0500

    [STRYV-10] Add calendar date picker component

--- src/components/custom/CalendarCard.tsx
+++ src/components/custom/CalendarCard.tsx
@@ -5,12 +5,44 @@ import { Calendar } from "@/components/custom/Calendar";
 import { Button } from "../ui/button";
 import { Card } from "../ui/card";
 import { Calendar as CalendarIcon } from "lucide-react";
+import { CalendarDatePicker } from "./CalendarDatePicker";
 
 export default function CalendarCard() {
   const [date, setDate] = React.useState<Date | undefined>(new Date());
+  const [selectedDateRange, setSelectedDateRange] = React.useState({
+    from: new Date(new Date().getFullYear(), 0, 1),
+    to: new Date(),
+  });
 
   return (
     <Card className="p-6 shadow-xs rounded-lg bg-linear-to-b from-white to-gray-50">
+      {/* Calendar Date Picker */}
+      <div className="p-4 max-w-xl">
+        <div className="flex justify-between items-center mb-4">
+          <span className="text-xl font-semibold text-gray-900 px-5 py-2 rounded-full shadow-xs">
+            Date Picker Component
+          </span>
+          <Button
+            variant="outline"
+            size="icon"
+            className="hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+          >
+            <CalendarIcon className="w-5 h-5 text-blue-600" />
+          </Button>
+        </div>
+        <CalendarDatePicker
+          date={selectedDateRange}
+          onDateSelect={setSelectedDateRange}
+        />
+        <div className="mt-4">
+          <h2 className="text-md font-semibold">Selected Date Range:</h2>
+          <p className="text-sm">
+            {selectedDateRange.from.toDateString()} -{" "}
+            {selectedDateRange.to.toDateString()}
+          </p>
+        </div>
+      </div>
+      {/* Calendar */}
       <div className="flex justify-between items-center mb-4">
         <span className="text-xl font-semibold text-gray-900 px-5 py-2 rounded-full shadow-xs">
           Schedule
@@ -24,9 +56,9 @@ export default function CalendarCard() {
         </Button>
       </div>
       <div className="rounded-lg bg-blue-50 p-4 flex flex-col items-center">
-        {/* <h3 className="text-lg font-semibold text-gray-800 mb-3">
+        <h3 className="text-lg font-semibold text-gray-800 mb-3">
           Select a Date
-        </h3> */}
+        </h3>
         <div className="w-full flex justify-center">
           <Calendar
             mode="single"
